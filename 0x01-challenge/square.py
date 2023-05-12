dba604a1002d3fc4fd943e74efe3fba92ccb2d60
Timmygee21 <saytimmy@gmail.com>
2023-05-12 23:47:55 +0000

    square fixed

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
+""" Defines Square class."""
+
 
 class Square():
-      
-    width = 0 
-    height = 0 
-  
-    def __init__(self, *args, **kwargs): 
-        for key, value in kwargs.items(): 
-            setattr(self, key, value) 
-  
-    def area_of_my_square(self): 
-        """Area of the square""" 
-        return self.width * self.width 
-  
-    def perimeter_of_my_square(self): 
-        """Perimeter of the square"""
-        return (self.width * 2) + (self.height * 2) 
-  
-    def __str__(self): 
-        return "{}/{}".format(self.width, self.height) 
-  
-if __name__ == "__main__": 
-  
-    s = Square(width=12, height=9) 
-    print(s) 
-    print(s.area_of_my_square()) 
-    print(s.perimeter_of_my_square())
+    """ Square class."""
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """ Instantiation of class."""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """ Perimeter of the square."""
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ Printable representation of square object."""
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    """ Creates Square object."""
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
